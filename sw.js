a9e7019ceeee4851fcb76a3d3a10ebb25fb94d7f
Mantis <mantispaypalxfd@gmail.com>
2025-12-01 23:41:08 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      
+      return cache.addAll(["./", "./index.html", "./style.css", "./script.js", "./img/*.png", "./music/*.mp3"]);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
